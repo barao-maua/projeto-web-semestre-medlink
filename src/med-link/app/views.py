3dca1d8a020baf0b56c714057dafdb21988bf6f8
Rafael Avelar <rafaelavelar2005@hotmail.com>
2026-03-18 23:51:55 -0300

    E2: configuração de rotas, views e estrutura de templates

--- src/med-link/app/views.py
+++ src/med-link/app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
+
+def sobre(request):
+    return render(request, 'pages/sobre.html')
+
+def especialidades(request):
+    return render(request, 'pages/especialidades.html')
